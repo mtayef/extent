87d6c39f38683653b19a5e79d78d1bf1e94ad9b0
Tayef <Tayef@10.0.0.249>
2018-06-29 16:08:35 -0400

    first

---
+++ ExtentPrac/src/test/java/reportTutorial/ReportTut.java
@@ -0,0 +1,10 @@
+package reportTutorial;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.NetworkMode;
+
+public class ReportTut {
+	ExtentReports report = new ExtentReports("./rep/"+".html ", NetworkMode.OFFLINE);
+	
+	
+}
